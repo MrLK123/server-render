8d8968b74bdd37fb1bff60f8381625fe6b082f25
刘嘿哈 <mr_lk123@163.com>
2019-06-23 17:21:25 +0800

    create-react-app express 服务器端渲染

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
+
+
+export default class App extends  Component{
+constructor(){
+    super();
+    this.state={
+        list:["one","tow","throw"]
+    }
+}
+handleClick(){
+    alert("hello react");
+}
+ render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+         <h1 onClick={this.handleClick}>Hello react</h1>
+         <ul>
+             {this.state.list.map(item=>(
+                 <li>{item}</li>
+             ))}
+         </ul>
+     </div>
+  )
+ }
 }
 
-export default App;
